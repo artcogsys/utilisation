f0dccb2dc84ffee857054c1fd52cd1fa95b9c257
xarion <erdicalli@gmail.com>
2017-04-07 09:51:14 +0200

    addd per image standardization

--- classifier/pipeline.py
+++ classifier/pipeline.py
@@ -33,6 +33,7 @@ def read_and_decode_image_file(filename_queue):
     image = tf.image.decode_jpeg(value)
     shape = tf.shape(image)
     image = tf.cond(shape[1] < shape[0], lambda: tf.image.transpose_image(image), lambda: image)
+    image = tf.image.per_image_standardization(image)
     return tf.cast(image, dtype=tf.float32)
 
 
